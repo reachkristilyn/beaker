3d590c2677467531eea6d55693aac927fd14efe7
Kristi Lyn Eaton <reachkristi@gmail.com>
2026-08-05 20:11:20 -0400

    change swirl pic

--- src/data/wallProducts.ts
+++ src/data/wallProducts.ts
@@ -24,7 +24,7 @@ export const wallProducts: WallProduct[] = [
   {
     id: "swirlangle",
     name: "Swirl Angle",
-    image: "/products/swirlangle2.png",
+    image: "/products/swirlangle3.png",
     panelSizeInches: PANEL_SIZE_INCHES,
   },
 ];
