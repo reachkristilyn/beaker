520dac82c7124650f3e3bfb848bcf99482d939b1
Kristi Lyn Eaton <reachkristi@gmail.com>
2026-08-10 18:41:11 -0400

    redo angle add aspect

--- src/components/wall-designer/ProductPanel.tsx
+++ src/components/wall-designer/ProductPanel.tsx
@@ -38,7 +38,7 @@ export default function ProductPanel({ panel, product, selected, width, height, 
         style={{
             width: "100%",
             height: "100%",
-            objectFit: "fill",
+            objectFit: "contain",
             display: "block",
             transform: `rotate(${panel.rotation}deg)`,
           }}
